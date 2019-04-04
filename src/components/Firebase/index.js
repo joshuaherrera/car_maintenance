5c9d4fab72a77a13623bef9e964ff39e80c1b9eb
Joshua Herrera <herrjosh@oregonstate.edu>
2019-04-04 08:31:55 -0700

    added login functionality

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,8 +1,8 @@
-import FirebaseContext from './Context';
+import FirebaseContext, { withFirebase } from './Context';
 import Firebase from './Firebase';
 
 //exports all needed functionality for Firebase
 
 export default Firebase
 
-export { FirebaseContext };
+export { FirebaseContext, withFirebase };
