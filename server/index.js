8c03a3eedaced0155887de580303ffc71cb4450f
Joshua Herrera <herrjosh@oregonstate.edu>
2019-10-17 13:14:52 -0700

    continue oauth flow

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('./services/passport'); //executes passport config
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(new GoogleStrategy()); //define which strategy to use for auth
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
