85c85f5ba46288b8d6c55feb2be67e7cc833fe87
Joshua Herrera <herrjosh@oregonstate.edu>
2019-10-19 21:30:08 -0700

    added connection to postgres with sequelize

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const Sequelize = require('sequelize');
 require('./services/passport'); //executes passport config
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
 
+const sequelize = new Sequelize(keys.postgresURI, {
+    dialectOptions: {
+        ssl: true //needed for heroku
+    }
+});
+sequelize
+    .authenticate()
+    .then((err) => {
+        console.log('success');
+    })
+    .catch((err) => {
+        console.log('error', err);
+    });
 const app = express();
 
 app.use(express.json()); // no need for body-parser
